959343d5ccae1c03ee109ddca167960159520d1d
Michael Crosby <crosbymichael@gmail.com>
2017-01-27 11:26:42 -0800

    Update gistit to v3

--- main.go
+++ main.go
@@ -6,94 +6,92 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/Sirupsen/logrus"
-	"github.com/codegangsta/cli"
 	gh "github.com/crosbymichael/octokat"
-)
-
-var (
-	token  string
-	logger = logrus.New()
+	"github.com/urfave/cli"
 )
 
 // Load the users github token from
 // $HOME/.github
-func loadToken() (string, error) {
+func loadToken() string {
 	path := filepath.Join(os.Getenv("HOME"), ".github")
 	b, err := ioutil.ReadFile(path)
 	if err != nil {
-		return "", err
+		logrus.WithError(err).Fatal("load token")
 	}
-	return string(b), nil
+	return strings.Trim(string(b), "\n")
 }
 
 func newFile(r io.Reader) gh.File {
 	data, err := ioutil.ReadAll(r)
 	if err != nil {
-		logger.Fatal(err)
+		logrus.Fatal(err)
 	}
 	return gh.File{
 		Content: string(data),
 	}
 }
 
-func getFilesFromContext(context *cli.Context) map[string]gh.File {
+func getFilesFromContext(context *cli.Context) (map[string]gh.File, error) {
 	if len(context.Args()) == 0 {
 		return map[string]gh.File{
 			"stdin": newFile(os.Stdin),
-		}
+		}, nil
 	}
 	files := make(map[string]gh.File)
 	for _, file := range context.Args() {
 		f, err := os.Open(file)
 		if err != nil {
-			logger.Fatal(err)
+			return nil, err
 		}
 		name := filepath.Base(file)
 		files[name] = newFile(f)
 		f.Close()
 	}
-	return files
+	return files, nil
 }
 
 func main() {
 	app := cli.NewApp()
 	app.Name = "gistit"
 	app.Usage = "post content to gist.github.com"
-	app.Version = "2"
+	app.Version = "3"
 	app.Author = "@crosbymichael"
 	app.Flags = []cli.Flag{
-		cli.BoolFlag{Name: "secret,s", Usage: "set the gist to secret"},
-		cli.StringFlag{Name: "description,d", Usage: "set the gist's description"},
-		cli.StringFlag{Name: "token", Usage: "use the specified github access token"},
+		cli.BoolFlag{
+			Name:  "secret,s",
+			Usage: "set the gist to secret",
+		},
+		cli.StringFlag{
+			Name:  "description,d",
+			Usage: "set the gist's description",
+		},
+		cli.StringFlag{
+			Name:  "token",
+			Usage: "use the specified github access token",
+			Value: loadToken(),
+		},
 	}
-
-	app.Before = func(context *cli.Context) (err error) {
-		token = context.GlobalString("token")
-		if token == "" {
-			if token, err = loadToken(); err != nil {
-				return err
-			}
-		}
-		return nil
-	}
-
-	app.Action = func(context *cli.Context) {
+	app.Action = func(context *cli.Context) error {
 		client := gh.NewClient()
-		client.WithToken(token)
+		client.WithToken(context.String("token"))
 
-		files := getFilesFromContext(context)
+		files, err := getFilesFromContext(context)
+		if err != nil {
+			return err
+		}
 		gist, err := client.CreateGist(
 			context.GlobalString("description"),
 			!context.GlobalBool("secret"), files)
 		if err != nil {
-			logger.Fatal(err)
+			return err
 		}
 		fmt.Println(gist.HtmlUrl)
+		return err
 	}
-
 	if err := app.Run(os.Args); err != nil {
-		logger.Fatal(err)
+		logrus.Fatal(err)
 	}
 }
